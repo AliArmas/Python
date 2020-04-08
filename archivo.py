0643ceb0f4ce041b9a4f896448f9522394caf7e2
AliArmas <191287@ids.upchiapas.edu.mx>
2020-04-08 17:05:24 -0500

    calculadora en ifanidados

---
+++ archivo.py
@@ -0,0 +1,33 @@
+#primer programa en python
+#calculadora basica
+
+a = 1
+while a>=1 :
+    print("Calculadora")
+    print("1.-Sumar")
+    print("2.-Restar")
+    print("3.-Multiplicar")
+    print("4.-Salir")
+    a = intprint("Calculadora")
+print("1.-Sumar")
+print("2.-Restar")
+print("3.-Multiplicar")
+print("4.-Salir")
+a = int(input()) (input()) 
+while(a>=1):    
+    print("Ingresa el primer numero")
+    numero = int(input())
+    print("Ingresa el segundo numero")
+    numero2 = int(input())
+    if (a == 1):
+        print("El resultado es:", numero+numero2)
+    elif (a == 2):
+        print("El resultado es:", numero-numero2)
+    elif (a == 3):
+        print("El resultado es: ", numero*numero2)
+    elif (a == 4):
+        a = 0 
+print("terminé")
+        
+    
+
